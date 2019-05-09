1b44d598b4bd4ac7a79638e4a8bc92a009d1c74a
AMutaa <piusandru@gmail.com>
2019-05-08 23:29:46 -0400

    init

--- initialization.py
+++ initialization.py
@@ -1,2 +1,12 @@
-name = input('What is your team name? ')
-favourite_ide = input('What is your favourite IDE? ')
+# BMI CALCULATOR
+
+age_bracket = 20
+
+# SIZES
+
+small = 'Underweight'
+medium = 'Normal or Healthy Weight'
+large = 'Overweight'
+xlarge = 'Obese'
+
+weight_in_kilos = 68
