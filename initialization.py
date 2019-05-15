d65b6a1c9774b5874b43fbb2cfcaf698c6214a25
AMutaa <piusandru@gmail.com>
2019-05-15 16:22:58 -0400

    data calculations of weight and height

--- initialization.py
+++ initialization.py
@@ -15,3 +15,24 @@ height_metres = 1.65
 one_kilo = 0.453592
 one_foot = 12
 one_meter = 0.0254
+
+# CHANGE WEIGHT TO KGS
+
+
+def lbstokgs(user_weight):
+    weightInKgs = user_weight * one_kilo
+    return weightInKgs
+
+
+# CHANGE FEET TO INCHES
+
+def feetToInches(users_feet):
+    users_height = users_feet * one_foot
+    return users_height
+
+# INCHES TO METERS
+
+
+def inchesToMeters(users_feet, users_inches):
+    total_user_height = ((feetToInches(users_feet) + users_inches) * one_meter)
+    return total_user_height
